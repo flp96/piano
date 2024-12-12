9f791b5ef6923f190dce564673b0326be9884ab6
flp96 <filipefernandes__@live.com>
2024-12-12 17:00:34 -0300

    controle de volume adicionado

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,4 +1,5 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const volumeSlider = document.querySelector(".volume-slider input");
 
 let mapedKeys = [];
 
@@ -26,3 +27,11 @@ document.addEventListener("keydown", (e) => {
   playTune(e.key);
   }
 });
+
+const handleVolume = (e) => {
+  audio.volume = e.target.value;
+}
+
+volumeSlider.addEventListener("input", handleVolume);
+
+
